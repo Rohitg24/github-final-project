4557ec374323e71a48d99a8f2651ac9c14015224
Rohitg24 <54299999+Rohitg24@users.noreply.github.com>
2022-10-24 14:17:48 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,8 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Author: Rohit Gholap
+   # <Rohitg24>
 
    # Input:
    # p, principal amount
